8da21c72be61bc01a0807c62cad925b68ed6b9ee
hirwa9 <hirwawilly9@gmail.com>
2025-04-05 23:37:25 +0200

    Implement cast screen and show status bar by defauls

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import { colors } from '@/constants/colors';
 
 export default function RootLayout() {
   return (
@@ -17,10 +18,14 @@ export default function RootLayout() {
           name="movies/[id]"
           options={{ headerShown: false }}
         />
+        <Stack.Screen
+          name="cast/[id]"
+          options={{ headerShown: false }}
+        />
       </Stack>
       <StatusBar
-        hidden={true}
-        backgroundColor="#030014"
+        // hidden={true}
+        backgroundColor={colors.primary}
         style="light"
       />
     </SafeAreaView>
