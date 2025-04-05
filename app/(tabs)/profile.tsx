7304fd39030e88a4310b46557f6f2b6b26b9a5b0
hirwa9 <hirwawilly9@gmail.com>
2025-04-05 23:38:44 +0200

    Replace hard-coded color constants

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,13 @@
 import { Image, Text, View } from 'react-native';
 import React from 'react';
 import { icons } from '@/constants/icons';
+import { colors } from '@/constants/colors';
 
 const Profile = () => {
     return (
         <View className='bg-primary flex-1 px-10'>
             <View className='flex-1 flex flex-col items-center justify-center gap-5'>
-                <Image source={icons.person} className='size-10' tintColor="#fff" />
+                <Image source={icons.person} className='size-10' tintColor={colors.white} />
                 <Text className='text-gray-500 text-base'>Profile</Text>
             </View>
         </View>
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,13 +1,14 @@
 import { Image, Text, View } from 'react-native';
 import React from 'react';
 import { icons } from '@/constants/icons';
+import { colors } from '@/constants/colors';
 
 const Saved = () => {
     return (
 
         <View className='bg-primary flex-1 px-10'>
             <View className='flex-1 flex flex-col items-center justify-center gap-5'>
-                <Image source={icons.save} className='size-10' tintColor="#fff" />
+                <Image source={icons.save} className='size-10' tintColor={colors.white} />
                 <Text className='text-gray-500 text-base'>Saved</Text>
             </View>
         </View>
