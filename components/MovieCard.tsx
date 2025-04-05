bf23ec5dbf65a23e72530d4f3fbc01cb9ee79c69
hirwa9 <hirwawilly9@gmail.com>
2025-04-05 23:29:56 +0200

    Enhance UI

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,21 +5,19 @@ import { icons } from '@/constants/icons';
 
 const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
     return (
-        <Link href={`/movies/${id}`} asChild>
+        <Link href={`/movies/${id}`} asChild className='mb-3'>
             <TouchableOpacity className='w-[30%]'>
-                <Image
-                    source={{
-                        uri: poster_path ?
-                            `https://image.tmdb.org/t/p/w500${poster_path}` :
-                            'https://placehold.co/600x400/1a1a1a/ffffff.png'
-                    }}
-                    className='w-full h-52'
-                    style={{
-                        borderBottomWidth: 4,
-                        borderColor: '#1a3a8a',
-                    }}
-                    resizeMode='cover'
-                />
+                <View className='w-full border-b-4 border-purple-950'>
+                    <Image
+                        source={{
+                            uri: poster_path ?
+                                `https://image.tmdb.org/t/p/w500${poster_path}` :
+                                'https://placehold.co/600x400/1a1a1a/ffffff.png'
+                        }}
+                        className='w-full h-52'
+                        resizeMode='cover'
+                    />
+                </View>
                 <Text className='mt-2 text-sm font-bold text-white' numberOfLines={1}>{title}</Text>
                 <View className='flex-row items-center justify-start gap-1'>
                     <Image source={icons.star} className='size-4 ' />
@@ -27,8 +25,8 @@ const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie
                         {Math.round(vote_average / 2)}
                     </Text>
                 </View>
-                <View className='flex-row items-center justify-between'>
-                    <Text className='mt-1 text-xs text-light-300 font-medium'>
+                <View className='flex-row items-center justify-between mt-1'>
+                    <Text className='text-xs text-light-300 font-medium'>
                         {(release_date?.split('-')[0])}
                     </Text>
                     <Text className='text-xs text-light-300 font-medium uppercase'>
