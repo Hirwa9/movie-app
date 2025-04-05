b5028fc3225e0eab22e33d3efd9f0159d9d69740
hirwa9 <hirwawilly9@gmail.com>
2025-04-05 23:31:45 +0200

    Provide corresponding cast routing

--- components/CastCard.tsx
+++ components/CastCard.tsx
@@ -1,5 +1,6 @@
-import { Image, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
+import { Link } from 'expo-router';
 
 interface CastCardProps {
     id: number,
@@ -10,21 +11,23 @@ interface CastCardProps {
 
 const CastCard = ({ id, name, character, profile_path }: CastCardProps) => {
     return (
-        <View className='items-center w-[5rem] overflow-hidden'>
-            <Image
-                source={{
-                    uri: profile_path ?
-                        `https://image.tmdb.org/t/p/w500${profile_path}` :
-                        'https://placehold.co/600x400/1a1a1a/ffffff.png'
-                }}
-                resizeMode='cover'
-                className='w-14 h-14 rounded-full mb-2'
-            />
-            <View className='w-full'>
-                <Text className='text-center text-white' numberOfLines={1}>{name}</Text>
-                <Text className='text-center text-light-200 text-xs' numberOfLines={1}>{character}</Text>
-            </View>
-        </View>
+        <Link href={`/cast/${id}`} asChild>
+            <TouchableOpacity className='items-center w-[5rem] overflow-hidden'>
+                <Image
+                    source={{
+                        uri: profile_path ?
+                            `https://image.tmdb.org/t/p/w500${profile_path}` :
+                            'https://placehold.co/600x400/1a1a1a/ffffff.png'
+                    }}
+                    resizeMode='cover'
+                    className='w-14 h-14 rounded-full mb-2'
+                />
+                <View className='w-full'>
+                    <Text className='text-center text-white' numberOfLines={1}>{name}</Text>
+                    <Text className='text-center text-light-200 text-xs' numberOfLines={1}>{character}</Text>
+                </View>
+            </TouchableOpacity>
+        </Link>
     )
 }
 
