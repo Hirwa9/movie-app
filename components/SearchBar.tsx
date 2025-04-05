65d9a1e718627c68188037ec21d16083a63457bf
hirwa9 <hirwawilly9@gmail.com>
2025-04-05 23:35:32 +0200

    Replace hard-coded color constants

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,7 @@ import { View, Image, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { icons } from '@/constants/icons';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { colors } from '@/constants/colors';
 
 interface SearchBarProps {
     value?: string,
@@ -29,15 +30,15 @@ const SearchBar = ({ placeholder, value, clearValue, onPress, onChangeText, clas
 
     return (
         <View className={`relative flex-row items-center px-5 bg-dark-200 rounded-full focus-within:scale-105 transition-transform duration-200 ${className}`}>
-            <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff" />
+            <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={colors.accent} />
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder} value={value}
                 onChangeText={onChangeText}
-                onChange={() => { }} placeholderTextColor="#ab8bff" className='flex-1 ml-2 py-3 text-white outline-none'
+                onChange={() => { }} placeholderTextColor={colors.accent} className='flex-1 ml-2 py-3 text-white outline-none'
             />
             {canClearValue && (
-                <Ionicons name="close-outline" size={20} color="#ab8bff" className="absolute right-4 top-1/2 -translate-y-1/2"
+                <Ionicons name="close-outline" size={20} color={colors.accent} className="absolute right-4 top-1/2 -translate-y-1/2"
                     onPress={clearValue}
                 />
             )}
