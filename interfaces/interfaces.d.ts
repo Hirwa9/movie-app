92cda8c0eabe5f8aca9e86d28ea4954a3603a5f1
hirwa9 <hirwawilly9@gmail.com>
2025-04-07 22:18:48 +0200

    Add tv series fetch APIs

--- interfaces/interfaces.d.ts
+++ interfaces/interfaces.d.ts
@@ -1,6 +1,7 @@
 interface Movie {
   id: number;
-  title: string;
+  title?: string;
+  name?: string;
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
@@ -13,6 +14,7 @@ interface Movie {
   video: boolean;
   vote_average: number;
   vote_count: number;
+  media_type: 'movie' | 'tv';
 }
 
 interface TrendingMovie {
@@ -71,6 +73,88 @@ interface MovieDetails {
   vote_count: number;
 }
 
+interface TvSeriesDetails {
+  adult: boolean;
+  backdrop_path: string | null;
+  created_by: {
+    id: number;
+    credit_id: number;
+    name: string;
+    gender?: number;
+    profile_path?: string;
+  } | null;
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string | null;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: {
+    id: number;
+    name: string;
+    overview: string;
+    air_date: string;
+    episode_number: number;
+    production_code: string;
+    runtime: number;
+    season_number: number;
+    show_id: number;
+    still_path: string | null;
+    vote_average: number;
+    vote_count: number;
+  };
+  name: string;
+  next_episode_to_air: null;
+  networks: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_name: string;
+  original_language: string;
+  overview: string | null;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  seasons: {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string | null;
+    season_number: number;
+  }[];
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string | null;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
 interface MovieCredits {
   id: number;
   cast: any[],
@@ -110,4 +194,5 @@ interface SimiralMovies {
 interface TrendingCardProps {
   movie: any;
   index: number;
+  media_type: string;
 }
